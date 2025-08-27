1a868e11c855dc3ac0f36b0e0f0e402d7eda3a4c
unknown <deltaworldanimaux@gmail.com>
2025-08-27 16:27:21 +0100

    ugsxghn

--- models/Order.js
+++ models/Order.js
@@ -19,27 +19,27 @@ const orderSchema = new mongoose.Schema({
   },
   suppliesList: { type: String, required: true },
   status: { 
-  type: String, 
-  default: 'pending',
-  enum: ['pending', 'confirmed', 'processing', 'ready', 'delivered']
-},
+    type: String, 
+    default: 'pending',
+    enum: ['pending', 'confirmed', 'processing', 'ready', 'delivered']
+  },
   orderNumber: { type: String, required: true, unique: true },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Library' },
   cost: { type: Number, default: null },
-  refusalReason: { type: String, default: null }, // Add refusal reason field
+  refusalReason: { type: String, default: null },
   deliveryMan: { type: mongoose.Schema.Types.ObjectId, ref: 'DeliveryMan' },
-deliveryStatus: { 
-  type: String, 
-  default: 'pending',
-  enum: ['pending', 'assigned', 'picked_up', 'delivered']
-},
-deliveryCost: { type: Number, default: null },
-rejectedBy: [{
+  deliveryStatus: { 
+    type: String, 
+    default: 'pending',
+    enum: ['pending', 'assigned', 'picked_up', 'delivered']
+  },
+  deliveryCost: { type: Number, default: null },
+  rejectedBy: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'DeliveryMan'
   }]
 }, {
-  timestamps: true
+  timestamps: true 
 });
 
 orderSchema.index({ location: '2dsphere' });
